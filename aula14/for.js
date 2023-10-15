868bb0c2ba4b098a83eb86146742c2592071007d
Gabryel Adriano Borges de Souza <byelacademico@gmail.com>
2023-10-15 20:10:44 -0300

    aula14

---
+++ aula14/for.js
@@ -0,0 +1,11 @@
+// for (var c = 1; c <=10 ; c++) {
+//     console.log(c)
+// }
+
+// for (var c = 10 ; c != 0; c--) {
+//     console.log(c)
+// }
+
+for (var c = 10 ; c != 0; c -= 2) {
+    console.log(c)
+}
