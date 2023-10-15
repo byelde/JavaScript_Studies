c48ff155beadd448ba68a2e5c5e8fb23a65cbc43
Gabryel Adriano Borges de Souza <byelacademico@gmail.com>
2023-10-14 21:14:26 -0300

    aula11

---
+++ aula11/aula008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`Your velocity is ${vel}km/h`)
+
+if (vel > 40) {
+    console.log('You exceeded the speed limit.')
+}
---
+++ aula11/aula009.js
@@ -0,0 +1,9 @@
+var country = 'Brasil'
+
+console.log(`You are living in ${country}`)
+
+if (country == 'Brasil') {
+    console.log('You`re brazilian.')
+} else {
+    console.log('You aren`t brazilian.')
+}
